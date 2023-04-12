88d5c2fdbae0b979afeab000c772fc25c08b0b7a
Timothy A. DeWees <timothy.dewees@gmail.com>
2023-04-12 09:54:07 -0400

    Initial project setup

---
+++ cc_starter/tests/test_views.py
@@ -0,0 +1,13 @@
+from cc_starter.views.default import my_view
+from cc_starter.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'cc_starter'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
